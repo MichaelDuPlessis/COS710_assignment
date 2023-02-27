4283279ba3e5af3051f11b070e4f24ca7b664bcd
Michael du Plessis <mdp0101@gmail.com>
2023-02-27 20:04:11 +0200

    finished performance metrics code

--- performance_module/measures.py
+++ performance_module/measures.py
@@ -7,3 +7,24 @@ def rmse(predictions, targets):
     rmse = np.sqrt(mse)
     
     return rmse
+
+def r_squared(predictions, targets):
+    predictions = np.array(predictions)
+    targets = np.array(targets)
+    ss_residual = np.sum((targets - predictions)**2)
+    ss_total = np.sum((targets - np.mean(targets))**2)
+    r2 = 1 - (ss_residual / ss_total)
+    return r2
+
+
+def median_absolute_error(predictions, targets):
+    predictions = np.array(predictions)
+    targets = np.array(targets)
+    mae = np.median(np.abs(predictions - targets))
+    return mae
+
+def mean_absolute_error(predictions, targets):
+    predictions = np.array(predictions)
+    targets = np.array(targets)
+    mae = np.mean(np.abs(predictions - targets))
+    return mae
